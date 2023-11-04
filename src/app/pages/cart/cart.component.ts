f5d2d3465cc363e17f64c76b40d0344022b35bd8
Houseliofer <20030392@itcelaya.edu.mx>
2023-11-03 21:32:29 -0600

    Agregue logica del carrito

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Cart,CartItem } from 'src/app/models/cart.model'
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -17,7 +18,7 @@ export class CartComponent {
     product: 'https://via.placeholder.com/150',
     name: 'shoes',
     price: 150,
-    quantity: 1,
+    quantity: 3,
     id: 2,
   }
 ]};
@@ -32,13 +33,13 @@ export class CartComponent {
      'action'
   ];
 
+  constructor(private cartService:CartService){}
+
   ngOnInit(): void{
     this.dataSource = this.cart.items;
   }
 
   getTotal(items: Array<CartItem>):number{
-    return items.
-    map((item) => item.price * item.quantity)
-    .reduce((prev, current) => prev + current, 0)
+    return this.cartService.getTotal(items)
   }
 }
